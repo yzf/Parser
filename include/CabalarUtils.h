6eca72bb35f559fad8043c026828791aaf651fa4
yzf <375007501@qq.com>
2013-08-07 20:13:49 +0800

    Cabalar注释

--- include/CabalarUtils.h
+++ include/CabalarUtils.h
@@ -16,7 +16,7 @@ class Formulas;
  * 进行Cabalar转换的工具类
  */
 class CabalarUtils {
-public:
+private:
     static _formula* disjLeftTrue(_formula* _originalFml);
     static _formula* disjLeftFalse(_formula* _originalFml);
     static _formula* conjLeftTrue(_formula* _originalFml);
@@ -26,21 +26,22 @@ public:
     static _formula* implRightTrue(_formula* _originalFml);
     static _formula* implRightFalse(_formula* _originalFml);
     
-    static Formulas leftRule_1(_formula* _originalFml);
-    static Formulas leftRule_2(_formula* _originalFml);
-    static Formulas leftRule_3(_formula* _originalFml);
-    static Formulas leftRule_4(_formula* _originalFml);
-    static Formulas leftRule_5(_formula* _originalFml);
+    static Formulas* leftRule_1(_formula* _originalFml);
+    static Formulas* leftRule_2(_formula* _originalFml);
+    static Formulas* leftRule_3(_formula* _originalFml);
+    static Formulas* leftRule_4(_formula* _originalFml);
+    static Formulas* leftRule_5(_formula* _originalFml);
     
-    static Formulas rightRule_1(_formula* _originalFml);
-    static Formulas rightRule_2(_formula* _originalFml);
-    static Formulas rightRule_3(_formula* _originalFml);
-    static Formulas rightRule_4(_formula* _originalFml);
-    static Formulas rightRule_5(_formula* _originalFml);
+    static Formulas* rightRule_1(_formula* _originalFml);
+    static Formulas* rightRule_2(_formula* _originalFml);
+    static Formulas* rightRule_3(_formula* _originalFml);
+    static Formulas* rightRule_4(_formula* _originalFml);
+    static Formulas* rightRule_5(_formula* _originalFml);
     
     static int commutaiveTrans(_formula* _root, _formula* _cur, 
                         FORMULA_TYPE _commutativeSymbol);
-    static Formulas transform(_formula* _originalFml);
+public:
+    static Formulas* transform(_formula* _originalFml);
 };
 
 
