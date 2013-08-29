7da846eac090195c8089679ca3124af29d326ffa
yzf <375007501@qq.com>
2013-08-29 23:17:05 +0800

    benchmark完成

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
+if [ $# -ne 2 ]
 then
-    echo "usage: $0 data_size"
-    echo "example: $0 5"
+    echo "usage: $0 data_size flag   (flag != 0 to output the answer set)"
+    echo "example: $0 5 0"
     exit 1
 fi
 
@@ -11,6 +11,7 @@ priCircInputFile="pri_input/benchmark_$1.in"
 priCircFactFile="pri_fact/benchmark_$1.fact"
 circ2dlpInputFile="circ2dlp_input/benchmark_$1.lp"
 tmpFile=".tmp"
+result="result"
 
 if [ ! -f $priCircInputFile -o ! -f $circ2dlpInputFile ]
 then
@@ -20,24 +21,24 @@ fi
 
 touch $tmpFile
 # 运行pricirc
-echo -n "running priCirc ......"
+echo -n "running priCirc ......" > $result
 priCirc $priCircInputFile $tmpFile > /dev/null
 if [ $2 -eq 1 ]
 then
-    time gringo $priCircFactFile $tmpFile | claspD 0
+    { time gringo $priCircFactFile $tmpFile | claspD 0; } >> $result 2>> $result
 else
-    time gringo $priCircFactFile $tmpFile | claspD 0 > /dev/null
+    { time gringo $priCircFactFile $tmpFile | claspD 0; } > /dev/null 2>> $result
 fi
 
 echo
 # 运行circ2dlp
-echo -n "running circ2dlp ......"
+echo -n "running circ2dlp ......" >> $result
 gringo $circ2dlpInputFile > $tmpFile
 if [ $2 -eq 1 ]
 then
-    time circ2dlp $tmpFile -m "abx* : abo* : aba*" -v "ina* inb* out*" | claspD 0
+    { time circ2dlp $tmpFile -m "abx* : abo* : aba*" -v "ina* inb* out*" | claspD 0; } >> $result 2>> $result
 else
-    time circ2dlp $tmpFile -m "abx* : abo* : aba*" -v "ina* inb* out*" | claspD 0 > /dev/null
+    { time circ2dlp $tmpFile -m "abx* : abo* : aba*" -v "ina* inb* out*" | claspD 0; } > /dev/null 2>> $result
 fi
 
 if [ -f $tmpFile ]
